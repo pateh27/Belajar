ef872f1b4949705fa77581fbaafd436d97451a92
pateh27 <fathzfl@gmail.com>
2023-09-04 16:43:45 +0700

    cocok

--- LearningGithub19.java
+++ LearningGithub19.java
@@ -2,5 +2,6 @@ public class LearningGithub19 {
     public static void main(String[] args) {
         System.out.println("Saya bisa bikin folder baru ");
         System.out.println("Mencoba materi baru");
+        
     }
 }
